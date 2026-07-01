f606f4eec4a4686f447c366bdbcc2d42ad983770
Luciano Marrocco <lucianomarrocco2@gmail.com>
2026-07-01 11:35:54 +0200

    change

--- esercizio 3/assets/main.js
+++ esercizio 3/assets/main.js
@@ -1,9 +1,9 @@
 console.log("js caricato");
 
 const bestBookEver = {
-    title: "L'isola degli enigmi",
-    author: "Loris Barbiero",
-    publicYear: 2024,
+    title: "Un Isola",
+    author: "Giovanni Boccaccio",
+    publicYear: 1800,
     type: [mistery, rompicapo, enigmi],
     target: "nobody",
     pages: 214,
